f8fa6c221bcf703f390e55484c7f829269d65c14
Ayrat <wair21@gmail.com>
2022-05-21 23:43:02 +0300

    add system color blue

--- iosTestApp/ViewController.swift
+++ iosTestApp/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
     }
     
     func setupView() {
-        // для PR поменял цвет на желтый, чтобы были изменения
-        view.backgroundColor = .systemYellow
+        // для PR поменял цвет, чтобы были изменения
+        view.backgroundColor = .systemBlue
     }
 
 
